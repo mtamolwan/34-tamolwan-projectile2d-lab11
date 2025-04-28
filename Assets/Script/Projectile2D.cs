5c51b9996dbfbfd93b5d286718889506c337712c
mtamolwan <tamolwan.nooc@bumail.net>
2025-04-29 01:21:41 +0700

    Add Script Projectile2D

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class Projectile2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletPrefab;
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //shoot raycast 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            //get click point
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray,Mathf.Infinity);
+
+            //hit object with collider
+            if ( hit.collider != null)
+            {
+                //show target
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit" + hit.collider.name);
+
+                //callculate projectile velocity
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position,hit.point,1f);
+
+                //shoot bullet prefab using rb2d
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+
+                //add projectile velocity vectorr to bullet rb
+                shootBullet.linearVelocity = projectileVelocity;
+            }
+
+        }
     }
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+
+        //find velocity of x and y
+        float velocityX = distance.x * time;
+        float velocityY = distance.y * time + 0.5f * Mathf.Abs(Physics2D.gravity.y)*time;
+
+        //get projectile vector
+        Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+        
+        return projectileVelocity ;
+    }
+
+
 }
